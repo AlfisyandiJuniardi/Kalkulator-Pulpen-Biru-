f654feda5ce1ef6f309a98af536b656673a733da
dhabit159 <62578082+dhabit159@users.noreply.github.com>
2020-04-14 21:52:29 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int saldo=0;
+	    int pilihan=0;
+        Kas k = new Kas();
+        //       Scanner keyboard = new Scanner(System.in);
+//        System.out.println("Catatan Keuangan");
+//        System.out.println("1.Pemasukan");
+//        System.out.println("2.Pengeluaran");
+//        System.out.println("3.Jumlah Saldo Sekarang");
+//        System.out.println("4.keluar");
+//        System.out.println("Masukkan Pilihan : ");
+        k.menu(pilihan,saldo);
+
+
+
+//        if(pilihan==1){
+//            System.out.println("PEMASUKAN");
+//            System.out.println("Masukkan Jumlah saldo = ");
+//            z = keyboard.nextInt();
+//            saldo=saldo+z;
+//            System.out.println("total  Saldo =  "+saldo);
+//        }
+//        else if(pilihan==2){
+//            System.out.println("PENGELUARAN");
+//            System.out.println("Masukan Jumlah Saldo = ");
+//            z = keyboard.nextInt();
+//            saldo=saldo-z;
+//            System.out.println("total saldo = "+saldo);
+//        }
+//        else if(pilihan==3){
+//            System.out.println("SALDO");
+//            System.out.println("Saldo = "+saldo);
+//        }
+//        else{
+//            System.out.println("Pilihan salah coba lagi ");
+//        }
+    }
+}
